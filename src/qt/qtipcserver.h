1847f7d74e06927d5cbb4f152c6af2b6367e5c06
Dominic Damoah <Dominic.imakestuff@gmail.com>
2017-08-09 13:43:46 +0000

    Readded files

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define ebullioncoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "ebullioncoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
